7e18edf2a1471f4dde4b2639bb1352baf5ba825f
taekjoo <taekjoo@gmail.com>
2015-03-10 01:02:22 +0900

    product create and index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,24 @@ class ProductsController < ApplicationController
   end
 
   def create
-    render plain: params[:product].inspect
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :brand, :category, :price)
+    end
+
+
 end
